27f913f7cc79ac26f36118536415a888602205fe
Calvin Wang <calvin.wang0628@gmail.com>
2016-07-11 14:59:57 -0700

    remembering login

--- workspace/sample_app/app/helpers/sessions_helper.rb
+++ workspace/sample_app/app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Rememberse a user in a persistent session 
+  def remember(user) 
+    user.remember
+    cookies.permanent.signed[:used_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end 
+
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
--- workspace/sample_app/app/models/user.rb
+++ workspace/sample_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -14,4 +15,19 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  def User.new_token 
+    SecureRandom.urlsafe_base64
+  end 
+
+  #remembers a user in the database for use rin persistent sessions 
+  def remember
+    self.remember_token = User.new_token 
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end 
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end 
+
 end
