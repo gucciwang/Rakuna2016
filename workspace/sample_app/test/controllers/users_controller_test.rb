c0b9cb50c916aec255faff6d9a6b1994b7c9d1ab
Calvin Wang <calvin.wang0628@gmail.com>
2016-07-13 15:10:16 -0700

    following users and micro posts

--- workspace/sample_app/test/controllers/users_controller_test.rb
+++ workspace/sample_app/test/controllers/users_controller_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-	def setup 
-		@user = users(:michael)
-		@other_user = users(:archer) 
-	end 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
 
   test "should redirect index when not logged in" do
     get users_path
@@ -57,5 +57,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
