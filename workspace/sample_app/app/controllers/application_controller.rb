36899e2ce814b315b3b4fafcac713a2f0b87e961
Calvin Wang <calvin.wang0628@gmail.com>
2016-07-13 13:13:16 -0700

    micro posts

--- workspace/sample_app/app/controllers/application_controller.rb
+++ workspace/sample_app/app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  private
 
-
-  #def hello
-  #	render html: "Hello, world!"
-  #end 
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
