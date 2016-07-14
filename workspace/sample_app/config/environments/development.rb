e69fa9f51268fc22b54fd3e13899cacf848201a9
Calvin Wang <calvin.wang0628@gmail.com>
2016-07-13 18:39:23 -0700

    fix host

--- workspace/sample_app/config/environments/development.rb
+++ workspace/sample_app/config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.delivery_method = :test 
-  host = 'localhost:3000'
+  host = 'https://guarded-dawn-49506.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host, protocal: 'https'}
 
   config.action_mailer.perform_caching = false ### delete? 
